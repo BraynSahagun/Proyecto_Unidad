59af94dd3e25ed3e2ad55fd331b784afa3b6f6d8
Aiden Sahagun <78627580+BraynSahagun@users.noreply.github.com>
2022-07-12 19:19:23 -0500

    Agregar archivos de proyecto.

---
+++ Context/AplicationdbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto_Unidad_1_23BV.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_Unidad_1_23BV.Context
+{
+    internal class AplicationdbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySQL("server = localhost; database = proyectou1; user = root; password = ");
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ Modelo/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_Unidad_1_23BV.Modelo
+{
+    internal class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
